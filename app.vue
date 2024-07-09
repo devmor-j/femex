348494073d7ae2617903f2d50c872f57ec14afd6
Morteza Jamshidi <devmor.j@gmail.com>
2024-07-09 18:58:13 +0000

    update orderbook on each ws message and display a unstyled simple orderbook

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <script setup>
-const { orderbookRef, tradeRef, connect } = usePhemexContractWebSocket();
+const { orderbookComputed, tradesComputed, connect } =
+  usePhemexContractWebSocket();
 
 onMounted(() => {
   connect();
@@ -7,7 +8,31 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <h1>Hello Femex</h1>
+  <div class="m-8">
+    <div class="w-fit mx-auto">
+      <div class="text-red-500">
+        <div
+          v-for="ask in orderbookComputed.asks.slice(20)"
+          class="flex gap-4 justify-center"
+        >
+          <span class="min-w-[6ch]">{{ ask[0] }}</span>
+          <span>|</span>
+          <span class="min-w-[6ch]">{{ ask[1] }}</span>
+        </div>
+      </div>
+
+      <hr class="my-2" />
+
+      <div class="text-green-500">
+        <div
+          v-for="bid in orderbookComputed.bids.slice(0, 10)"
+          class="flex gap-4 justify-center"
+        >
+          <span class="min-w-[6ch]">{{ bid[0] }}</span>
+          <span>|</span>
+          <span class="min-w-[6ch]">{{ bid[1] }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- composables/usePhemexContractWebSocket.js
+++ composables/usePhemexContractWebSocket.js
@@ -1,20 +1,80 @@
+// TODO: extract to utility
+const toUnScaledNumbers = (n) => {
+  const price = n[0] / 1e4;
+  return [price, Math.floor(1e4 * (n[1] / price)) / 1e4];
+};
+
 export default function ({ symbol = "BTCUSD" } = {}) {
   const orderbookRef = ref({ asks: [], bids: [] });
-  const tradeRef = ref([[]]);
+  const tradesRef = ref([[]]);
+
+  const orderbookComputed = computed(() => ({
+    asks: orderbookRef.value.asks.map(toUnScaledNumbers).reverse(),
+    bids: orderbookRef.value.bids.map(toUnScaledNumbers),
+  }));
+  const tradesComputed = computed(() => tradesRef.value);
 
   const onMessage = {
     orderbook: (msg) => {
       if (msg.type === "snapshot") {
         orderbookRef.value = msg.book;
       } else if (msg.type === "incremental") {
-        // TODO: update orderbookRef
+        for (const ask of msg.book.asks) {
+          if (ask[1] === 0) {
+            for (const [index, oAsk] of orderbookRef.value.asks.entries()) {
+              if (ask[0] === oAsk[0]) {
+                orderbookRef.value.asks.splice(index, 1); // deletion
+                break;
+              }
+            }
+          } else {
+            if (ask[0] > orderbookRef.value.asks.at(-1)[0]) {
+              orderbookRef.value.asks.push(ask); // last insertion
+            } else {
+              for (const [index, oAsk] of orderbookRef.value.asks.entries()) {
+                if (ask[0] === oAsk[0]) {
+                  orderbookRef.value.asks[index][1] = ask[1]; // updation
+                  break;
+                } else if (ask[0] < oAsk[0]) {
+                  orderbookRef.value.asks.splice(index, 0, ask); // insertion
+                  break;
+                }
+              }
+            }
+          }
+        }
+
+        for (const bid of msg.book.bids) {
+          if (bid[1] === 0) {
+            for (const [index, oBid] of orderbookRef.value.bids.entries()) {
+              if (bid[0] === oBid[0]) {
+                orderbookRef.value.bids.splice(index, 1); // deletion
+                break;
+              }
+            }
+          } else {
+            if (bid[0] < orderbookRef.value.bids.at(-1)[0]) {
+              orderbookRef.value.bids.push(bid); // last insertion
+            } else {
+              for (const [index, oBid] of orderbookRef.value.bids.entries()) {
+                if (bid[0] === oBid[0]) {
+                  orderbookRef.value.bids[index][1] = bid[1]; // updation
+                  break;
+                } else if (bid[0] > oBid[0]) {
+                  orderbookRef.value.bids.splice(index, 0, bid); // insertion
+                  break;
+                }
+              }
+            }
+          }
+        }
       }
     },
-    trade: (msg) => {
+    trades: (msg) => {
       if (msg.type === "snapshot") {
-        tradeRef.value = msg.trades;
+        tradesRef.value = msg.trades;
       } else if (msg.type === "incremental") {
-        // TODO: update tradesRef
+        // TODO: update tradeRef
       }
     },
   };
@@ -27,13 +87,13 @@ export default function ({ symbol = "BTCUSD" } = {}) {
 
       if (!("error" in msg)) {
         if ("book" in msg) onMessage.orderbook(msg);
-        else if ("trades" in msg) onMessage.trade(msg);
-      } else {
-        // TODO: handle error (maybe toast bad network or reconnect)
+        else if ("trades" in msg) onMessage.trades(msg);
       }
     });
 
     ws.addEventListener("open", () => {
+      console.log(`[33mWS OPEN[0m`);
+
       const pingMsg = JSON.stringify({
         id: 0,
         method: "server.ping",
@@ -62,5 +122,5 @@ export default function ({ symbol = "BTCUSD" } = {}) {
     });
   };
 
-  return { orderbookRef, tradeRef, connect };
+  return { orderbookComputed, tradesComputed, connect };
 }
