b485fd81abf8b97206c6237b4a5c31f3e73a8e96
Morteza Jamshidi <devmor.j@gmail.com>
2024-07-11 13:31:13 +0000

    add page transition and move font to main stylesheet

--- app.vue
+++ app.vue
@@ -1,11 +1,22 @@
 <script setup></script>
 
 <template>
-  <div class="font-HarmonyOS_Sans">
+  <div>
     <NuxtPage></NuxtPage>
   </div>
 </template>
 
 <style lang="scss">
 @use "assets/style/main.scss";
+
+.page-enter-active,
+.page-leave-active {
+  transition-duration: 200ms;
+  transition-property: filter opacity;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-07-03",
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/fontaine"],
   app: {
+    pageTransition: { name: "page", mode: "out-in" },
     head: {
       meta: [{ name: "color-scheme", content: "dark light" }],
     },
